37604a0fcd870cf93754b198ced3553d99b30a15
Marlon Leandro Firmo <syfer.1616@gmail.com>
2022-12-16 03:12:35 -0300

    commit

--- sessao_4/143_dir_hasattr_e_getattr.py
+++ sessao_4/143_dir_hasattr_e_getattr.py
@@ -1,3 +1,22 @@
 string = 'luiz'
+metodo = 'upper'
 
 print(string)
+
+
+# checa se existe o método upper na variavel que tem um valor str
+if hasattr(string, metodo):
+    print('Existe upper')
+    # print(string.upper())
+    print(getattr(string, metodo)()) #obtem o metodo escrito na variavel "metodo"
+    # print(string)
+else:
+    print('Não existe o método', metodo)
+
+"""
+Dica: hasattr checa se existe um metodo, então, com getattr se obtem este metodo
+neste caso, checou se em string havia o metodo upper escrito na variavel metodo
+então descobriu-se que sim "True", logo o getattr obteve o metodo (converteu o texto
+dentro da variavel "metodo" em um metodo real e executou o upper e escreveu o nome em
+maiúsculo
+"""
---
+++ sessao_4/144_iterables_e_iterators.py
@@ -0,0 +1,16 @@
+# Generator expression, Iterables e Iterators em Python
+iterable = ['Eu', 'Tenho', '__iter__']
+iterator = iterable.__iter__() #tem __iter__ e __next__
+iterator = iter(iterable) # O mesmo comando de cima, só que mais facil de ler
+
+# iteraveis tem a responsabilidade de deter os valores
+# a unica responsabilidade do iterador é te entregar um valor por vez, ele só sabe
+# qual o próximo valor, ele não sabe qual é o primeiro, o anterior, o ultimo valor.
+
+
+# Os comandos abaixo o iterator não sabe trabalhar, acessar indice, ou usar o len
+# print(iterator[1])
+# print(len(iterator))
+
+# Ele só sabe usar o next
+print(next(iterator))
