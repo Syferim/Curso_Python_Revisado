ec9b2e9db39fa89f9b21333820feb41214ff5fad
Marlon Leandro Firmo <syfer.1616@gmail.com>
2022-12-19 01:39:04 -0300

    commit

---
+++ sessao_4/153_modularizacao_entendendo_os_seus_proprios_modulos_e_sys.path.py
@@ -0,0 +1,2 @@
+import aula_153
+print('Este módulo se chama', __name__)
---
+++ sessao_4/154_como_importar_coisas_do_proprio_modulo.py
@@ -0,0 +1,5 @@
+from aula_153 import soma, variavel_modulo
+print('Este módulo se chama', __name__)
+
+print(variavel_modulo)
+print(soma(2, 3))
